b8fab9f33d26c90c7798409305dbd2573495d0c7
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2025-01-31 02:26:21 +0530

    context api mini project

---
+++ 08miniproject/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+     <h1>Context API with react</h1>
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniproject/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext
+//we have to make a context provider along with the context by wrapping every component by <UserContext>
+
