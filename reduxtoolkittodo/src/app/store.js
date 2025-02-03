d24bdc5b29ce58193b5882e8daa497c2242afe76
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2025-02-03 20:59:17 +0530

    redux toolkit  todo project

---
+++ reduxtoolkittodo/src/app/store.js
@@ -0,0 +1,8 @@
+// step1 : import configureStore from reduxjs/toolkit
+import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+
+export const store = configureStore({
+    reducer:todoReducer
+}) 
